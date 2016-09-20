73b9d8b2d78b586b03a7ba9b88280c7ab1da51b0
oakes <zsoakes@gmail.com>
2016-09-20 10:17:25 -0400

    Add exercise 02

--- src/Email.java
+++ src/Email.java
@@ -2,5 +2,15 @@
  * Created by zach on 9/20/16.
  */
 public class Email {
+    String destination;
+    String subject;
+    String body;
+    boolean isSpam;
 
+    public Email(String destination, String subject, String body, boolean isSpam) {
+        this.destination = destination;
+        this.subject = subject;
+        this.body = body;
+        this.isSpam = isSpam;
+    }
 }
