3194f197e1181e295308a1cb7bcce68fea9c5d67
oakes <zsoakes@gmail.com>
2016-10-14 09:54:34 -0400

    Add exercise 6

---
+++ src/Exercise06.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zach on 10/14/16.
+ */
+public class Exercise06 {
+    public static void main(String[] args) {
+        isPalindrome("racecar"); // true
+        isPalindrome("hello"); // false
+    }
+
+    public static boolean isPalindrome(String str) {
+        return false;
+    }
+}
